f278706c0b0041ce6fe0837afad2956390679f70
Jack <naaturalmaster@gmail.com>
2016-07-23 12:23:56 +0800

    新增数据库帮助类，以及各表的实体类

---
+++ app/src/main/java/com/example/zimmerman/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.zimmerman.coolweather.model;
+
+/**
+ * Created by Zimmerman on 2016/7/23.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
